ca6af7eef51bd2ed5de51982e6776615b087e039
Avighan <dpn.majumder@gmail.com>
2014-09-21 01:23:18 +0530

    Cacheinversematrix

---
+++ CacheInversematrix.R
@@ -0,0 +1,24 @@
+makeCacheMatrix <- function(x = numeric()){
+  m <- NULL
+  set <- function(y){
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(solve) m <<- solve(m)
+  getinverse <- function() m
+  list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
+}
+
+
+cacheSolve <- function(x = matrix()){
+  m <- NULL
+  set <- function(y){
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(solve) m <<- solve(m)
+  getinverse <- function() m
+  list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
+}
